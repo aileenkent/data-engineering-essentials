72800f5970c83a07fdfc0123b44dab83b98db132
Aileen Kent <aileenmkent27@gmail.com>
2025-09-17 16:19:33 -0400

    in class group work

--- synthetic/clean-data.py
+++ synthetic/clean-data.py
@@ -17,7 +17,7 @@ def clean_parquet():
                 AS
             SELECT * FROM read_parquet('{input_file}');
         """)
-
+            
         """ More cleaning steps
         1. Add age column and populate it (see transform/README.md)
         2. Delete rows with NULL for the 'score' column
@@ -25,7 +25,39 @@ def clean_parquet():
         4. Max age? Min age? How many over 100?
         5. How many records are left?
         """
+        con.execute(f"""
+             ALTER TABLE synthdata 
+                ADD COLUMN age INTEGER;
+             UPDATE sythdata 
+                SET age = date_diff('year', birth_date, CURRENT_DATE);""")
+        
+        con.execute(f"""
+            DELETE FROM synthdata
+                WHERE score IS NULL;""")
 
+        con.execute(f"""
+            -- Create a new table with unique rows
+            CREATE TABLE sythdata_clean AS 
+            SELECT DISTINCT * FROM sythdata;
+            -- Drop original and rename
+            DROP TABLE sythdata;
+            ALTER TABLE sythdata_clean RENAME TO sythdata;""")
+        
+        con.execute(f"""
+            --max age? min age? trying to print them
+            SELECT MAX(age) as max_age, MIN(age) as min_age;
+            COUNT AS over_100 
+                FROM sythdata 
+                WHERE age OVER 100;
+            RETURN(f" Max age: {max_age}");
+            RETURN(f" Min age: {min_age}");
+            RETURN(f" Over 100: {over_100}");
+            """)
+        
+        con.execute(f"""
+            SELECT COUNT(*) AS total_records
+            RETURN(f" Records left: {total_records}");
+            """)
 
     except Exception as e:
         print(f"An error occurred: {e}")
